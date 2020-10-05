75ff1bdb51a81e63ccc743e4c57485878da4e5aa
xLocalName <xLocalEmail@gmail.com>
2020-10-05 16:31:17 +0300

    Добавлены тесты "создания контакта"

---
+++ contact.py
@@ -0,0 +1,22 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homenumber,
+                 mobilenumber, worknumber, faxnumber, email1, email2, email3, homepage, address2, home2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homenumber = homenumber
+        self.mobilenumber = mobilenumber
+        self.worknumber = worknumber
+        self.faxnumber = faxnumber
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.address2 = address2
+        self.home2 = home2
+        self.notes = notes
