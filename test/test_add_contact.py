51b9bf6b844dfddcf104488872eddb77e0331107
xLocalName <xLocalEmail@gmail.com>
2020-10-11 18:21:56 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,21 +11,21 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="fn", middlename="mn", lastname="ln", nickname="nn", title="t",
                                company="c",
                                address="a", homenumber="1", mobilenumber="2", worknumber="3", faxnumber="4",
                                email1="m1",
                                email2="m2", email3="m3", homepage="h", address2="sa",
                                home2="sh", notes="sn"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                address="", homenumber="", mobilenumber="", worknumber="", faxnumber="",
                                email1="",
                                email2="", email3="", homepage="", address2="",
                                home2="", notes=""))
-    app.logout()
+    app.session.logout()
