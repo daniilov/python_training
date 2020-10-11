3050c998594df170dfd196e9bf994321a7b24831
xLocalName <xLocalEmail@gmail.com>
2020-10-11 09:31:17 +0300

    Декомпозиция кода, классы-помощники

--- application2.py
+++ application2.py
@@ -17,6 +17,7 @@ class Application:
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
