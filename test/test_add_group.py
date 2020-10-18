26633205def3f3e039a9e72f93ed927f6de0e960
xLocalName <xLocalEmail@gmail.com>
2020-10-18 15:27:23 +0300

    Загружаем информацию из тестируемого приложения в виде списков(Группы)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,8 +2,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="create group", header="create logo", footer="create comment"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
