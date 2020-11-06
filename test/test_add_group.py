710b4e6b9eb0454bc66d6b72531b00fcb3efde8e
xLocalName <xLocalEmail@gmail.com>
2020-11-06 15:34:03 +0300

    добавлена работа с jasonpickle для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,11 @@
 from model.group import Group
-import pytest
-from data.groups import constant as testdata
 
-def test_add_group(app, data_groups):
-    group = data_groups
+
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
