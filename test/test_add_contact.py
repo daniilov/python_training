ed51e9591727191a19b85c9da286e1eba05553d7
xLocalName <xLocalEmail@gmail.com>
2020-11-06 15:46:59 +0300

    добавлена работа с jasonpickle для контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,26 +1,11 @@
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", address="", homephone="", email="")] + [
-    Contact(firstname=random_string("firstname", 5), lastname=random_string("lastname", 5),
-            address=random_string("address", 5), homephone=random_string("homephone", 5),
-            email=random_string("email", 5))
-    for i in range(5)]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == app.contact.count()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
